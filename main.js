877e590df4c421b58294634c0078708cdd68b448
Priscila Schleske <plasch7@gmail.com>
2023-08-14 15:21:49 -0600

    adds word and checks for word

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css';
 import getRandomWord from './src/randomWord.js';
 import { setSharkImage } from './src/sharkImage';
+import { setupWord, isLetterInWord, revealLetterInWord } from './src/word.js'
 
 document.querySelector('#app').innerHTML = `
   <section id="shark-img"></section>
@@ -12,11 +13,16 @@ document.querySelector('#app').innerHTML = `
   <section id="letter-buttons"></section>
 `;
 
+
 const initSharkwords = () => {
   let numWrong = 0;
   const word = getRandomWord();
   setSharkImage(document.querySelector("#shark-img"), numWrong);
+  setupWord(document.querySelector("#word-container"), word)
+  // console.log(isLetterInWord(word[0]), '(should be true)');
+  // console.log(isLetterInWord('1'), '(should be false)');
   // for debugging:
+  // reveaLetterInWord(word[0]);
   console.log(`[INFO] Correct word is: ${word}`);
 };
 
--- src/word.js
+++ src/word.js
@@ -1,7 +1,23 @@
 let word;
 
-function setupWord(initWord, element) {}
+function setupWord(element, initWord) {
+    word = initWord;
+    word.split('').forEach(() => {
+        element.insertAdjacentHTML('beforeend', `<div class="letter-box"></div>`);
+    });
+}
 
-function isLetterInWord(letter) {}
+function isLetterInWord(letter) {
+    return word.includes(letter);
+}
 
-function revealLetterInWord(letter) {}
+function revealLetterInWord(letter) {
+    const letterBoxes = document.querySelectorAll(".letter-box");
+    word.split('').forEach((wordLetter, idx) => {
+        if (wordLetter === letter) {
+            letterBoxes[idx].innerHTML = letter;
+        }
+    });
+}
+
+export { setupWord, isLetterInWord, revealLetterInWord };
