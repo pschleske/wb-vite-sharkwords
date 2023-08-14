2f5be3f020af81e6ff11acc76ea995aa6c3a71b6
Priscila Schleske <plasch7@gmail.com>
2023-08-14 16:12:47 -0600

    handleguess

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import './style.css';
 import getRandomWord from './src/randomWord.js';
-import { setSharkImage } from './src/sharkImage';
+import { setSharkImage } from './src/sharkImage.js';
 import { setupWord, isLetterInWord, revealLetterInWord } from './src/word.js'
+import setupGuesses from './src/guess.js';
 
 document.querySelector('#app').innerHTML = `
   <section id="shark-img"></section>
@@ -13,7 +14,6 @@ document.querySelector('#app').innerHTML = `
   <section id="letter-buttons"></section>
 `;
 
-
 const initSharkwords = () => {
   let numWrong = 0;
   const word = getRandomWord();
@@ -24,6 +24,34 @@ const initSharkwords = () => {
   // for debugging:
   // reveaLetterInWord(word[0]);
   console.log(`[INFO] Correct word is: ${word}`);
+
+
+  const handleGuess = (guessEvent, letter) => {
+    const button = guessEvent.target;
+    button.setAttribute("disabled", true);
+    if (isLetterInWord(letter)) {
+      revealLetterInWord(letter);
+    } else {
+      numWrong += 1;
+      setSharkImage(sharkImEl, numWrong)
+    }
+  }
+  // let isWordComplete = true;
+  // for (const el of document.querySelectorAll('.letter-box')) {
+  //   if (el.innerText === '') {
+  //     isWordComplete = false;
+  //     break; // break will exit the loop
+  //   } else {
+  //     document.querySelectorAll('button').forEach((btn) => {
+  //       btn.setAttribute('disabled', true);
+  //     });
+  //   }
+  // }
+
+
+  setupGuesses(document.querySelector("#letter-buttons"), handleGuess);
 };
+
+
 
 initSharkwords();
--- src/guess.js
+++ src/guess.js
@@ -1,12 +1,19 @@
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
 function setupGuesses(element, handleGuess) {
+  // loop over each letter in the alphabet string
   alphabet.split('').forEach((letter) => {
+    // create a button element
     const btn = document.createElement('button');
+    // Set its innerText so it displays a letter
     btn.innerText = letter;
+    // attach the given event handler (handleguess) to the 'click' event
     btn.addEventListener('click', (e) => handleGuess(e, letter));
+    // append the button to the given element
     element.append(btn);
   });
 }
 
 export default setupGuesses;
+
+// The setupGuesses function will create a button for every letter in the alphabet and attach an event handler to it. When the user clicks on a button, it’ll trigger the button’s event handler function. The handleGuess function is called with two arguments: the event object and the letter that was guessed.
