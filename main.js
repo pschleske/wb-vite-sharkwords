c0dc0a24e537baa2c9c3cb6709e06b2aaa73126f
Priscila Schleske <plasch7@gmail.com>
2023-08-14 14:47:15 -0600

    adds images

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css';
 import getRandomWord from './src/randomWord.js';
+import { setSharkImage } from './src/sharkImage';
 
 document.querySelector('#app').innerHTML = `
   <section id="shark-img"></section>
@@ -14,7 +15,7 @@ document.querySelector('#app').innerHTML = `
 const initSharkwords = () => {
   let numWrong = 0;
   const word = getRandomWord();
-
+  setSharkImage(document.querySelector("#shark-img"), numWrong);
   // for debugging:
   console.log(`[INFO] Correct word is: ${word}`);
 };
